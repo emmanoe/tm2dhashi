1cfb987726baa9fb5deb7f33a7142bdbf0939345
emmanoé <emmanoe.delar@etu.u-bordeaux.fr>
2016-11-05 14:38:27 +0100

    ajout fonctionalités ponts non avancées !

--- hashi_text.c
+++ hashi_text.c
@@ -16,14 +16,26 @@ int max_x_y(int nb_nodes, node nodes[]){ /* fonction qui calcul la position du n
    return max;
 }
 
+dir intostr(int i){ //Fonction qui traduit le choix int en dir str !
+   dir d;
+   if (i==1)
+      d =WEST;
+   if (i==2)
+      d= EAST;
+   if (i==3)
+      d= NORTH;
+   if (i==4)
+      d= SOUTH;
+return d;
+
+}
+
                                          // PARTIE PRINTF INSTANCE DE JEU//
 void game_print(int nb_nodes,game g, node nodes[]){ /* Affiche l'instance de jeu créé */ // RAJOUT DE game g !!
    int exit = 0;
-   int ivoisini=-1;
    //int numnode;
    int *pc=&exit;
    //int *p_n=&numnode;
-   int *p_i=&ivoisini;
    int choix =0;
    int coordx=0; //Ajout de ponts entre 2 nodes
    int coordy=0;
@@ -42,43 +54,61 @@ void game_print(int nb_nodes,game g, node nodes[]){ /* Affiche l'instance de jeu
                i = game_get_node_number(g,x,y);
                printf(" %d ",get_required_degree(nodes[i]));
             }
-            else
-               printf("   ");
+            else if(x>0 && game_get_node_number(g,x-1,y)!=-1) //Factoriser ce code en fonction car répété à ligne 77 !!!
+               if (get_degree_dir(g,game_get_node_number(g,x-1,y),EAST)==0)
+                  printf("   ");
 
 
                //PARTIE PONT
 
-            if (game_get_node_number(g,x,y)!=-1){
-               if (get_degree(g,game_get_node_number(g,x,y))>= 1){
-                  if  (get_degree(g,game_get_node_number(g,x,y)) ==1)
-                     if (choix == 2 || choix == 1){{
-                        if (game_get_node_number(g,x,y)!=ivoisini){
-                           printf("-----"); //si degré >= 1 on dessine le pont
-                           *p_i = get_neighbour_dir(g,i,EAST);
-                        }
-                        else printf("     ");
-                        }
-                     }
-                  if (get_degree(g,game_get_node_number(g,x,y))==2)
-                     if (choix ==2 ||choix ==  1){{
-                           *p_i = get_neighbour_dir(g,i,EAST);
-                           if (game_get_node_number(g,x,y)!=ivoisini){
-                              printf("=====");
-                           }
-                           else printf("    5");
-                        }
-                     }
+            if (game_get_node_number(g,x,y)!=-1){ //on teste si le noeud est existant !
+               if (get_degree_dir(g,game_get_node_number(g,x,y),EAST)==1)/*choix == 2 || choix == 1*/{
+                        // if (game_get_node_number(g,x,y)!=ivoisini){
+                              if (x+1<max &&  game_get_node_number(g,x+1,y)==-1 )
+                                 printf("-------------"); //si degré >= 1 on dessine le pont
+                              else printf("-----");
                }
-               else if (get_degree(g,game_get_node_number(g,x,y))==0)
+
+               if (get_degree_dir(g,game_get_node_number(g,x,y),EAST)==2)
+               {{
+                     if (x+1<max &&  game_get_node_number(g,x+1,y)==-1 )
+                        printf("=============");
+                     else
+                        printf("=====");
+                  }
+                     //else printf("    5");
+                  }
+
+                  else if (get_degree_dir(g,game_get_node_number(g,x,y),EAST)==0)
                printf("     "); //suivi d'espace pour l'esthétique
             }
-            else printf("     ");
+            else if (game_get_node_number(g,x,y)==-1){
+               if(x>0 && game_get_node_number(g,x-1,y)!=-1)
+                  if (get_degree_dir(g,game_get_node_number(g,x-1,y),EAST)==0)
+                     printf("     ");
+            }
          }
-         *p_i = -1; // on réinitialise ivoisini
          printf("\n");
+         for (int x =0;x<max;x++){
+            if (game_get_node_number(g,x,y)!=-1 && game_get_node_number(g,x,y-1)!=-1){
+               if (get_degree_dir(g,game_get_node_number(g,x,y),SOUTH)==1 || get_degree_dir(g,game_get_node_number(g,x,y-1),NORTH)==1){
+                  printf(" |");
+                  printf("      ");
+               }
+               else if (get_degree_dir(g,game_get_node_number(g,x,y),SOUTH)==2){
+                  printf(" H");
+                  printf("      ");
+               }
+               else if (get_degree_dir(g,game_get_node_number(g,x,y),SOUTH)==0){
+                  printf("        ");
+               }
+            }
+            else if (game_get_node_number(g,x,y)==-1)
+               printf("      ");
+         }
          printf("\n");
-      }
-      //printf("\n");
+         }
+   //printf("\n");
 
                                                  // PARTIE INTERACTION//
 
@@ -95,8 +125,8 @@ void game_print(int nb_nodes,game g, node nodes[]){ /* Affiche l'instance de jeu
          printf("node numero %d\n",node_num);
 
 
-         if ( can_add_bridge_dir(g,node_num,EAST)){
-            add_bridge_dir(g,node_num,EAST);
+         if ( can_add_bridge_dir(g,node_num,intostr(choix))){
+            add_bridge_dir(g,node_num,intostr(choix));
 
          }
       }
@@ -130,7 +160,7 @@ int main(void){
    game g = new_game(7, nodes);
    //game_over(g);
    //game_node(g, 1);
-   game_print(7,g, nodes);
+   game_print(7,g, nodes);;
    //add_bridge(g,nodes);
    //max_x_y(nb_nodes,nodes);
    //delete_game(g);
