70ecb759962a1ac7aaf69b2aac4e68041d11e1c3
emmanoé <emmanoe.delar@etu.u-bordeaux.fr>
2016-12-08 11:56:49 +0100

    test_game.2 V1 complet

--- test_game2.c
+++ test_game2.c
@@ -36,7 +36,7 @@ bool test_new_game() {
    for (int i = 0 ; i < EXAMPLE_NB_NODE; i++){
       nodes[i] = new_node(vals[i][0],vals[i][1],vals[i][2]);
    }
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,4,4);
    bool r = true;;
 
    for (int i=0;i<EXAMPLE_NB_NODE;i++){ // On vérifie si chaque ile correspond au jeu passé en paramètre !!!
@@ -57,7 +57,7 @@ bool test_new_game() {
 }
 
 bool test_delete_game() {
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,4,4);
    bool r=true;
    delete_game(g);
    r = test_equality_bool(true,r,"pb new game") && r;
@@ -69,7 +69,7 @@ bool test_delete_game() {
 
 bool test_copy_game() {
    bool r=true;
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,4,4);
    game cpg = copy_game(g);
 
    for (int i =0;i<EXAMPLE_NB_NODE;i++){
@@ -86,7 +86,7 @@ bool test_copy_game() {
 
 
 bool test_game_nb_nodes(){
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,4,4);
    bool r = true;
 
    r = test_equality_int(EXAMPLE_NB_NODE,game_nb_nodes(g),"pb nb_nodes") && r;
@@ -97,7 +97,7 @@ bool test_game_nb_nodes(){
 }
 
 bool test_game_node() {
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,4,4);
    node n;
    bool r = true;
 
@@ -114,7 +114,41 @@ bool test_game_node() {
    return r;
 }
 
-/*bool test_game_over(){}*/
+bool test_game_over(){
+   // Example node fonction game over
+
+   int vals[4][3] = {{0,0,2},{0,1,2},{1,0,2},{1,1,2}};
+   node nodes[4];
+
+   bool r = true;
+
+   for (int i = 0 ; i < 4; i++){
+      nodes[i] = new_node(vals[i][0],vals[i][1],vals[i][2]);
+   }
+
+   game g = new_game(4,nodes,2,4); // (int nb_nodes, node *nodes, int nb_max_bridges, int nb_dir)
+
+   if (can_add_bridge_dir(g, 0, EAST)){
+
+      add_bridge_dir(g, 0, EAST);
+      add_bridge_dir(g, 0, EAST);
+
+   }
+
+   if (can_add_bridge_dir(g, 1, EAST)){
+
+      add_bridge_dir(g, 1, EAST);
+      add_bridge_dir(g, 1, EAST);
+
+   }
+
+   r = test_equality_bool(true, game_over(g),"pb game_over") && r;
+
+
+   *p = bilan(r);
+   return r;
+
+  }
 
 bool test_add_bridge(){
    game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
@@ -188,7 +222,7 @@ bool test_del_bridge(){
 }
 
 bool test_get_degree_dir (){
-      game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+      game g = new_game(EXAMPLE_NB_NODE,nodes,2,4);
    bool r = true;
    for (int i = 0 ; i < EXAMPLE_NB_NODE; i++) {
       int num = game_get_node_number (g, vals[i][0], vals[i][1]);
@@ -220,12 +254,37 @@ bool test_get_degree_dir (){
 
 
 
-/*bool test_get_degree(){
-  }*/
+bool test_get_degree(){
+
+   game g = new_game(EXAMPLE_NB_NODE,nodes,2,4);
+   bool r = true;
+
+   for (int i = 0 ; i < EXAMPLE_NB_NODE; i++) {
+      int num = game_get_node_number (g, vals[i][0], vals[i][1]);
+
+      if (can_add_bridge_dir(g, num,EAST)){ // Rajout de pont pour tester le degree
+         add_bridge_dir(g, num, EAST);
+      }
+      if (can_add_bridge_dir(g, num,NORTH)){
+         add_bridge_dir(g, num, NORTH);
+      }
+      if (can_add_bridge_dir(g, num,NORTH)){
+         add_bridge_dir(g, num, NORTH);
+      }
+   }
+
+    r = test_equality_int(3, get_degree(g, 0),"Problème get_degree node num 0 ");
+
+
+    delete_game(g);
+    *p = bilan(r);
+    return r;
+
+  }
 
 ///////// test_game_1////////
 bool test_get_node_number() {
-   game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
+   game g = new_game(EXAMPLE_NB_NODE,nodes,2,4);
    bool r = true;
 
    for (int i = 0 ; i < EXAMPLE_NB_NODE; i++) {
@@ -259,7 +318,7 @@ bool test_can_add_bridge_dir() {
 bool test_get_neighbour_dir() {
    game g = new_game(EXAMPLE_NB_NODE,nodes,9,4);
     bool r = true;
-    r = r && test_equality_int(5, get_neighbour_dir(g,0,EAST), "pb neighbour 0 EAST");
+    r = r && test_equality_int(5, get_neighbour_dir(g,6,SOUTH), "pb neighbour 6 SOUTH");
     r = r && test_equality_int(4, get_neighbour_dir(g,2,EAST), "pb neighbour 2 EAST");
     r = r && test_equality_int(0, get_neighbour_dir(g,1,SOUTH), "pb neighbour 1 SOUTH");
     r = r && test_equality_int(2, get_neighbour_dir(g,1,NORTH), "pb neighbour 1 NORTH");
@@ -279,6 +338,7 @@ int main (int argc, char *argv[])
    result = test_new_game() && result; // EFFECTUE TOUT LES TESTS !
    result = test_game_nb_nodes() && result;
    result = test_game_node() && result;
+   result = test_game_over() && result;
    result = test_delete_game() && result;
    result = test_copy_game() && result;
    result = result && test_get_neighbour_dir();
@@ -286,6 +346,7 @@ int main (int argc, char *argv[])
    result = result && test_get_node_number();
    result = result && test_del_bridge();
    result = result && test_get_degree_dir();
+   result = result && test_get_degree();
    result = result && test_can_add_bridge_dir();
    if (result){
       printf ("Les tests se sont déroulés normalement, bilan %d/13\n",cmpt);
